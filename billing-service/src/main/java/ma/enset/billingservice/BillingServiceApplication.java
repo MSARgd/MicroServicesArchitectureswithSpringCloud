94d65b09f658b04585aa53a160f31be63604add3
sidiAbdalla <sidabdallard@gmail.com>
2023-10-26 04:06:16 +0100

    Thu Oct 26 04:06:16 AM +01 2023

--- billing-service/src/main/java/ma/enset/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/ma/enset/billingservice/BillingServiceApplication.java
@@ -1,14 +1,30 @@
 package ma.enset.billingservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.vault.core.VaultTemplate;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class BillingServiceApplication {
+	@Autowired
+	public VaultTemplate vaultTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillingServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(){
+		return args -> {
+			vaultTemplate.opsForVersionedKeyValue("secret")
+					.put("keypair", Map.of("privKey","jkdhfkds","pubKey","uiyeiuwr"));
+		};
 	}
 
 }
